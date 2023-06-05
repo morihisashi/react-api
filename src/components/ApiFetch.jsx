09eeb0909c4add43843cbd7156d20452d97c2fce
morihisashi <hisashi.mori@humanage.co.jp>
2023-06-05 18:53:10 +0900

    中断

--- src/components/ApiFetch.jsx
+++ src/components/ApiFetch.jsx
@@ -2,21 +2,26 @@ import React, { useEffect, useState } from 'react'
 
 const ApiFetch = () => {
     const [getApi, setGetApi] = useState([]);
+    const [maxTemperature, setMaxTemperature] = useState([]);
 
     useEffect(() => {
-        fetch(
-            'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo',
-            { method: 'GET' })
-            .then(res => res.json())
-            .then(data => {
-                setGetApi(data)
-            })
-    }, [])
+        (async () => {
+            const res = await fetch(
+                'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo'
+            );
+            const data = await res.json();
+            setGetApi(data['daily']);
+            // console.log(data['daily']);
+        })();
+    }, []);
+    // setMaxTemperature(getApi);
+    console.log(getApi['temperature_2m_max']);
 
-    console.log(getApi);
     return (
         <div>
-            Hello
+            <ul>
+                {maxTemperature.map((data, id) => <li key={id}>{data}</li>)}
+            </ul>
         </div>
     )
 }
