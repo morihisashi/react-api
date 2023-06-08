72a6dd60ebd084f4f82ce222684b642d6b6c4175
morihisashi <hisashi.mori@humanage.co.jp>
2023-06-08 16:35:40 +0900

    いらないファイルの削除、api周りのロジックを修正

--- src/components/ApiFetch.jsx
+++ src/components/ApiFetch.jsx
@@ -3,26 +3,29 @@ import React, { useEffect, useState } from 'react'
 const ApiFetch = () => {
     const [getApi, setGetApi] = useState([]);
     const [maxTemperature, setMaxTemperature] = useState([]);
+    const [minTemperature, setMinTemperature] = useState([]);
+    const [times, setTimes] = useState([]);
 
     useEffect(() => {
-        (async () => {
-            const res = await fetch(
-                'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo'
-            );
-            const data = await res.json();
-            setGetApi(data['daily']);
-            // console.log(data['daily']);
-        })();
         setMaxTemperature(getApi['temperature_2m_max']);
-    }, []);
-    
-    // console.log(getApi['temperature_2m_max']);
+        setMinTemperature(getApi['temperature_2m_min']);
+        setTimes(getApi['time']);
+    }, [getApi]);
+
+    const temInfo = async () => {
+        const res = await fetch(
+            'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo'
+        );
+        const data = await res.json();
+        setGetApi(data['daily']);
+    }
 
     return (
         <div>
+            <h1>天気情報</h1>
+            <button onClick={() => temInfo()}>東京の情報を見る</button>
             <ul>
-                {/* {maxTemperature.map((data, id) => <li key={id}>{data}</li>)} */}
-                {maxTemperature}
+                {(maxTemperature) && (Object.values(maxTemperature).map((data, id) => <li key={id}>{data}</li>))}
             </ul>
         </div>
     )
