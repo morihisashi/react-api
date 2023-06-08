644d61e0128882ba53924343ae73ddab6c5c7daa
morihisashi <hisashi.mori@humanage.co.jp>
2023-06-08 17:25:55 +0900

    降水確率を追加

--- src/components/ApiFetch.jsx
+++ src/components/ApiFetch.jsx
@@ -6,16 +6,19 @@ const ApiFetch = () => {
     const [maxTemperature, setMaxTemperature] = useState([]);
     const [minTemperature, setMinTemperature] = useState([]);
     const [times, setTimes] = useState([]);
+    const [precipitation, setPrecipitation] = useState([]);
 
     useEffect(() => {
         setMaxTemperature(getApi['temperature_2m_max']);
         setMinTemperature(getApi['temperature_2m_min']);
         setTimes(getApi['time']);
+        setPrecipitation(getApi['precipitation_probability_max']);
     }, [getApi]);
 
     const temInfo = async () => {
         const res = await fetch(
-            'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo'
+            'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m,rain&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,precipitation_hours,precipitation_probability_max&timezone=Asia%2FTokyo'
+            // 'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min'
         );
         const data = await res.json();
         setGetApi(data['daily']);
@@ -27,20 +30,32 @@ const ApiFetch = () => {
             <button onClick={() => temInfo()}>東京の情報を見る</button>
             <table className='weather'>
                 <thead>
-                    <tr>
-                        日にち
-                        {(times) && (Object.values(times).map((data, id) => <th key={id}>{data}</th>))}
-                    </tr>
+                    {(times) && (
+                        <tr>
+                            日にち
+                            {Object.values(times).map((data, id) => <th key={id}>{data}</th>)}
+                        </tr>
+                    )}
                 </thead>
                 <tbody>
-                    <tr>
-                        最高気温
-                        {(maxTemperature) && (Object.values(maxTemperature).map((data, id) => <th key={id}>{data}度</th>))}
-                    </tr>
-                    <tr>
-                        最低気温
-                        {(minTemperature) && (Object.values(minTemperature).map((data, id) => <th key={id}>{data}度</th>))}
-                    </tr>
+                    {(maxTemperature) && (
+                        <tr>
+                            最高気温
+                            {Object.values(maxTemperature).map((data, id) => <th key={id}>{data}度</th>)}
+                        </tr>
+                    )}
+                    {(minTemperature) && (
+                        <tr>
+                            最低気温
+                            {Object.values(minTemperature).map((data, id) => <th key={id}>{data}度</th>)}
+                        </tr>
+                    )}
+                    {(precipitation) && (
+                        <tr>
+                            降水確率
+                            {Object.values(precipitation).map((data, id) => <th key={id}>{data}%</th>)}
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </div>
